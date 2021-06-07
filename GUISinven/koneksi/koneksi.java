5d8ee1bca716b449c3b59ef313f4201b2c415e23
Mar'atus Sholihah <80057770+Maratussholihah@users.noreply.github.com>
2021-06-07 08:20:16 +0700

    Add files via upload

---
+++ GUISinven/koneksi/koneksi.java
@@ -0,0 +1,33 @@
+// Nama : Mar'atus Sholihah
+// NIM  : 11200930000011
+// Kelas : 2A-Sistem Informasi
+
+package koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author asus
+ */
+public class koneksi {
+private Connection koneksi;
+    
+    public Connection connect(){
+     try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Berhasil Koneksi");
+    } catch (ClassNotFoundException ex){
+            System.out.println("Gagal Koneksi" + ex);
+    }
+    String url = "jdbc:mysql://localhost:3306/db_luki_silk";
+    try{
+    koneksi = DriverManager.getConnection(url, "root", "");
+            System.out.println("Berhasil Koneksi Database");
+    } catch (SQLException ex){
+            System.out.println("Gagal Koneksi Database" + ex);
+    }
+    return koneksi;
+    }       
+}    
+
